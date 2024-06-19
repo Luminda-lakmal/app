c942e0f9c2459f720969cd1dd52c558404c03d46
Luminda-lakmal <desilvaudll.19@uom.lk>
2024-06-19 20:26:18 +0530

    Resgister ui

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { AuthRoutingModule } from './auth-routing.module';
+import { NbThemeModule, NbLayoutModule, NbInputModule, NbButtonModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +14,13 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule,
+    NbInputModule,
+    NbButtonModule,
+    NbCardModule,
+    NbLayoutModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = `${environment.backendUrl}/api`;
+
+  constructor(private http: HttpClient) { }
+
+  // login() {
+  //   return this.http.post(`${this.apiUrl}/login`);
+  // }
+  register(object: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`,object);
+  }
+}
