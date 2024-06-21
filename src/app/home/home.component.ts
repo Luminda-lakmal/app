257c547e515e6a574a0c961aca6cd83b260e5a21
Luminda-lakmal <desilvaudll.19@uom.lk>
2024-06-22 00:45:00 +0530

    home page done

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../course/course.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  courses: any = []
+  constructor(
+    private courseService: CourseService,
+  ) { }
+
+  ngOnInit(): void {
+    this.courseService.getAllCourses().subscribe(
+      response => {
+        this.courses = response;
+        console.log('Available courses', this.courses);
+      },
+      error => {
+        console.error('Registration error', error);
+      }
+    )
+  }
+
+}
